0187a9f70a330d9b74db7330fe23819cb35a4410
Roman Jędras <romanjedras@o2.pl>
2019-07-22 21:31:05 +0200

    MERN blog task1

---
+++ client/src/components/layouts/MainLayout/MainLayout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../../features/NavBar/NavBar';
+
+
+
+const MainLayout = ({ children }) => (
+	<div>
+		<NavBar />
+		{children}
+	</div>
+);
+
+export default MainLayout;
