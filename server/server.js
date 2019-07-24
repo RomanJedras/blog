030466554c8efe76c4b654cd926fba68de2d74d2
Roman Jędras <romanjedras@o2.pl>
2019-07-24 16:26:04 +0200

    Add change project post by serve data from mongoDB

--- server/server.js
+++ server/server.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const mongoose = require('mongoose');
 // import routes
 const postRoutes = require('./routes/post.routes');
-
+const loadTestData = require('./testData');
+const helmet = require('helmet');
 
 dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace('' + '<dbuser>', process.env.USENAME).replace(''+'<dbpassword>',process.env.DATABASE_PASSWORD);
 
 
+ mongoose.connect(DB, {
+	useNewUrlParser: true
+});
+
+let db = mongoose.connection;
+
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+db.once('open', () =>
+	console.log('Connected to the database'),
+	loadTestData()
+);
+db.on('error', (err) => console.log('Error ' + err));
 const app = express();
-
+app.use(helmet());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
