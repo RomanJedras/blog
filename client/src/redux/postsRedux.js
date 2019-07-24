2b2e6a3d3927ef91612c5de83e81429c91e99cd9
Roman Jędras <romanjedras@o2.pl>
2019-07-24 12:09:00 +0200

    Add config file

--- client/src/redux/postsRedux.js
+++ client/src/redux/postsRedux.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { API_URL } from '../config';
 
 /* SELECTORS */
 export const getPosts = ({ posts }) => posts;
@@ -22,7 +23,7 @@ export const LOAD_POSTS = createActionName('LOAD_POSTS');
 export const loadPostsRequest = () => {
 	return dispatch => {
 		
-		axios.get('http://localhost:8000/api/posts').then(res => {
+		axios.get(`${API_URL}/posts`).then(res => {
 			dispatch(loadPosts(res.data));
 		})
 		.catch(err => {
