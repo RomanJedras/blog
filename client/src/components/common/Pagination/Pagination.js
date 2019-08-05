b5aa3c9926b76f9e3d09484a25f39fba2f2ce1a9
Roman Jędras <romanjedras@o2.pl>
2019-08-05 13:56:29 +0200

    Add Chevron to project

--- client/src/components/common/Pagination/Pagination.js
+++ client/src/components/common/Pagination/Pagination.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faChevronRight,
+	faChevronLeft
+} from "@fortawesome/free-solid-svg-icons";
 import './Pagination.scss';
 
 class Pagination extends Component {
@@ -15,16 +19,32 @@ class Pagination extends Component {
 		onPageChange(newPage);
 	}
 	
-	
+	skipToPage = increment => {
+		const { changePage } = this;
+		const { presentPage } = this.state;
+		const targetPage = presentPage + increment;
+		changePage(targetPage);
+	}
 	
 	render () {
 		const { pages } = this.props;
 		const { presentPage } = this.state;
-		const { changePage } = this;
+		const { changePage, skipToPage } = this;
 		
 		return (
 			<div className="pagination">
 				<ul className="pagination__list">
+					
+					{presentPage >= 2 && (
+						<li className="pagination__list__item">
+							<FontAwesomeIcon
+								icon={faChevronLeft}
+								onClick={() => {
+									skipToPage(-1);
+								}}
+							/>
+						</li>
+					)}
 					
 					{[...Array(pages)].map((el, page) =>
 						<li
@@ -32,6 +52,17 @@ class Pagination extends Component {
 							onClick={() => { changePage(page) }}
 							className={`pagination__list__item${((page) === presentPage) ? ' pagination__list__item--active' : ''}`}>
 							{page}
+						</li>
+					)}
+					
+					{presentPage !== pages && (
+						<li className="pagination__list__item">
+							<FontAwesomeIcon
+								icon={faChevronRight}
+								onClick={() => {
+									skipToPage(1);
+								}}
+							/>
 						</li>
 					)}
 				
