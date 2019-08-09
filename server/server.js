a05bf338e3d8d6a7555f4874b89cb6d04114c835
Roman Jędras <romanjedras@o2.pl>
2019-08-09 13:36:22 +0200

    first config by heroku

--- server/server.js
+++ server/server.js
@@ -2,26 +2,22 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 // import routes
+const path = require('path');
 const postRoutes = require('./routes/post.routes');
 const loadTestData = require('./testData');
-const helmet = require('helmet');
 const mongoSanitize = require("express-mongo-sanitize");
-
-
 
 
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace('' + '<dbuser>', process.env.USENAME).replace(''+'<dbpassword>',process.env.DATABASE_PASSWORD);
-
-
- mongoose.connect(DB, {
+	mongoose.connect(DB, {
 	useNewUrlParser: true
 });
 
 let db = mongoose.connection;
-
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 
@@ -35,8 +31,12 @@ app.use(mongoSanitize());
 app.use(helmet());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
+app.use("/api", postRoutes);
+app.use(express.static(path.join(__dirname, '/../client/build')));
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+});
 app.use(express.json());
-
 app.use('/api', postRoutes);
 
 const port = process.env.PORT || 3000;
